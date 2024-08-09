3596eb22445eb62a310d8a5d115eb4c91ee7292e
anilreddy-openbank <81958947+anilreddy-openbank@users.noreply.github.com>
2024-08-09 16:57:55 +0530

    test

--- mobile-auth.go
+++ mobile-auth.go
@@ -5,3 +5,7 @@ func Auth(userName string, password string) (map[string]string, error) {
 	user := map[string]string{"user_name": "sdsfdsd", "id": "1"}
 	return user, nil
 }
+
+func Work() string {
+	return "Called anil"
+}
